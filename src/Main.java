96e2e5f094f844c0150da2379041d305ca90663f
emrekabakci <emrekabakci.2@icloud.com>
2023-12-13 21:28:48 +0300

    Comments added.

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        //Variables defined
         double height, weight, result;
 
+        //New scanner created.
         Scanner inp = new Scanner(System.in);
 
+        //Data retrieved from user.
         System.out.print("Lütfen boyunuzu (metre cinsinden) giriniz : ");
         height = inp.nextDouble();
         System.out.print("Lütfen kilonuzu giriniz : ");
         weight = inp.nextDouble();
 
+        //Result variable set equal to body mass index.
         result = weight/(height*height);
 
+        //Result printed.
         System.out.println("Vucut kitle indeksiniz : " + result);
     }
 }
